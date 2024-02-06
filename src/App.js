f4551355bf0b8ee68832314456264beef76016ac
charliegale16 <charles.gale@csu.fullerton.edu>
2024-02-06 04:23:16 -0800

    added loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Questions from './components/Questions';
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
 
 
 const App = () => {
   
-  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an async task (e.g., fetching data) that takes some time
+    setTimeout(() => {
+      setLoading(false); // Set loading to false after the async task is done
+    }, 2500); // Simulate a 2-second loading time
+  }, []);
   
   return (
     <div className='App'>
-
-    <div className='top-section'>
-      <h1>Workout Generator</h1>
-      <p>Select your Targets: </p>
-      </div>
-      <Questions />
-
-
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className='top-section'>
+            <h1>Workout Generator</h1>
+            <p>Select your Targets: </p>
+          </div>
+          <Questions />
+        </>
+      )}
     </div>
-    
   );
-}
+};
 
 export default App
